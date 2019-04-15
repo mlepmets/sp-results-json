598028a58a07b7a0f4aa6b26d1fd3b137fd1f108
Martti Lepmets <mlepmets@gmail.com>
2019-04-15 23:57:16 +0300

    adjusted google font

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 };
 </script>
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,500');
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,500');
 $primaryGray: #e4e4e4;
 $hoverGray: #f5f5f5;
 .nav {
